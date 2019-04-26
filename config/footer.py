a68f0424d6082da943f024d5fdb44d61604f6d75
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2019-04-26 13:02:43 +0500

    Create footer.py

---
+++ config/footer.py
@@ -0,0 +1,20 @@
+"""
+  ___________________________                __ ________        
+ /   _____/\_____  \______   \__ __  _______/  |\_____  \______ 
+ \_____  \   _(__  <|    |  _/  |  \/  ___/\   __\_(__  <_  __ \
+ /        \ /       \    |   \  |  /\___ \  |  | /       \  | \/
+/_______  //______  /______  /____//____  > |__|/______  /__|   
+        \/        \/       \/           \/             \/       ~ An0n 3xPloiTeR
+"""
+
+from config import *
+
+footer = """
+ {white}[{green}${white}] {cyan}Thanks For Using {white}:D
+\t{blue}~ {red}An0n 3xPloiTeR {white}:)""".format(
+        white = w,
+        green = g,
+        cyan = c,
+        blue = b,
+        red = r
+    )
